9338ae9a88e1cc4625035df19fab030259cd98bc
Prince Mishra <princemishraa588@gmail.com>
2025-04-15 17:03:52 +0530

    Update selectionSort.java

--- Sorting/selectionSort.java
+++ Sorting/selectionSort.java
@@ -3,6 +3,7 @@ public class selectionSort {
         int arr[] = { 7, 8, 3, 1, 2 };
 
         // Selection Sort: This algorithm sorts an array by repeatedly finding the minimum element from the unsorted part and putting it at the beginning.
+        // Time complexity = O(n^2)
         for (int i = 0; i < arr.length - 1; i++) {
             // Outer loop iterates through the array, marking the beginning of the unsorted part. It goes up to the second last element.
             int smallest = i; // Assume the current element is the smallest in the unsorted part.
