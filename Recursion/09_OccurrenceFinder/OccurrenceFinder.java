085b588da18b0503c7664ad721b0a457c1a6f07b
Prince Mishra <princemishraa588@gmail.com>
2025-04-29 22:37:56 +0530

    Update OccurrenceFinder.java

--- Recursion/09_OccurrenceFinder/OccurrenceFinder.java
+++ Recursion/09_OccurrenceFinder/OccurrenceFinder.java
@@ -3,6 +3,7 @@
 public class OccurrenceFinder {
     // Static variables to store the indices of the first and last occurrences found so far.
     // Initialized to -1 to indicate the character hasn't been found yet.
+
     public static int first = -1;
     public static int last = -1;
 
@@ -10,9 +11,11 @@ public class OccurrenceFinder {
     Recursively finds the first and last occurrence of a character in a string.
     It scans the string from left to right using the index.
     
-    str The input string.
-    ele The character to find.
-    idx The current index being checked (starts from 0).
+    Time Complexity: O(n), Space Complexity: O(n)
+
+    str - The input string.
+    ele - The character to find.
+    idx - The current index being checked (starts from 0).
     */
     public static void getIndices(String str, char ele, int idx) {
         // Base Case: If the current index reaches the end of the string, stop recursion.
@@ -48,3 +51,7 @@ public class OccurrenceFinder {
         System.out.println("Last occurence : " + last);
     }
 }
+/*
+Output => First occurence : 1
+          Last occurence : 10
+*/
